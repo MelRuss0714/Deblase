7e6f7a7597cf5fda8f058961fde1b814881c2464
MelRuss0714 <37506211+MelRuss0714@users.noreply.github.com>
2018-08-19 14:37:19 -0400

    Update moods.js

--- models/moods.js
+++ models/moods.js
@@ -1,16 +1,38 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var Moods = sequelize.define("Moods", {
     mood: {
-        type: DataTypes.STRING,
-        // AllowNull is a flag that restricts a todo from being entered if it doesn't
-        // have a text value
-        allowNull: false,
-        // len is a validation that checks that our todo is between 1 and 140 characters
-        validate: {
-            len: [1, 250]
-        }
+      type: DataTypes.STRING,
+      // AllowNull is a flag that restricts a todo from being entered if it doesn't
+      // have a text value
+      allowNull: false,
+      // len is a validation that checks that our todo is between 1 and 140 characters
+      validate: {
+        len: [1, 250]
+      }
     }
-});
-    return Moods;
-  };
+    
+  });
+  Moods.create({  
+    mood: 'happy' 
+  });
+  Moods.create({  
+    mood: 'dreamy' 
+  });
+  Moods.create({  
+    mood: 'angry' 
+  });
+  Moods.create({  
+    mood: 'sad' 
+  });
+  Moods.create({  
+    mood: 'stressed' 
+  });
+  Moods.create({  
+    mood: 'weird' 
+  });
+  Moods.create({  
+    mood: 'bored' 
+  });
+  return Moods;
   
+};
